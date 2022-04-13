2398cf5f27897e46a9e174370a57e38273dc8395
Ingrzs <ingramirozs@gmail.com>
2022-04-13 12:06:22 -0500

    tercer script

---
+++ weekly_mission_2/ejemplo3.js
@@ -0,0 +1,19 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Tulio",
+    age: 2,
+    nicknames: [
+     "Tulipan",
+      "Tulancingo",
+      "Tulish"
+    ],
+    address: {
+      zip_code: "10000",
+      street: "Dr. Vertiz 11 Benito Juarez",
+      city: "CDMX"
+    }
+  }
+  console.log("Ejemplo 3: Objeto con diferentes propiedades")
+  console.log(myObject3)    //mostrara todos los valores del objeto
+  console.log(myObject3.name) //mostrar el objeto especifico que tiene object y el cual contiene name
+  console.log(myObject3["address"])   //mostrara todos los objetos que estan dentro de un arreglo ya que tiene corchete
