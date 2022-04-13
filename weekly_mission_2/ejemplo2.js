7fe6d8d6de685180be24b18c1080bc1bfd017410
Ingrzs <ingramirozs@gmail.com>
2022-04-13 12:44:07 -0500

    operadores

--- weekly_mission_2/ejemplo2.js
+++ weekly_mission_2/ejemplo2.js
@@ -3,6 +3,6 @@ const myObjetc2 = {
     name: "Carlo",
     age: 27
   }
-  console.log("Ejemplo 2: Crear un objeto con propiedades")
+  console.log("Ejemplo 2: Crear un objeto con propiedades") //mensaje que se mostrara en la consola
   console.log(myObjetc2)   //en la consola se mostrara todo los objetos que tiene myobject
   
