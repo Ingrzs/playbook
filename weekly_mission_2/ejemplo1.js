c41cfa6d74dff3e6715dabe899f71dd1d522df39
Ingrzs <ingramirozs@gmail.com>
2022-04-13 11:49:30 -0500

    primer script

---
+++ weekly_mission_2/ejemplo1.js
@@ -0,0 +1,3 @@
+const myObjetc = {} // Esto es un objeto vacío
+console.log("Ejemplo 1: Crear un objeto vacío")// se mostrara este mensaje
+console.log(myObjetc)  //solo se mostraran los parentesis porque miobject no tiene nada
